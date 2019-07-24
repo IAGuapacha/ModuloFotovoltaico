368ffc7cd35102ba4ceaefc17547f071f5b6d88e
IAGuapacha <iandresguapacha@gmail.com>
2019-07-23 19:33:17 -0500

    Getter y setter para la clase ViewHolder, nombre de la clase Sensor

--- app/src/main/java/com/example/modulofotovoltaico/GridAdapter.java
+++ app/src/main/java/com/example/modulofotovoltaico/GridAdapter.java
@@ -15,12 +15,12 @@ import static com.example.modulofotovoltaico.R.layout.adapter_sensor;
 public class GridAdapter extends BaseAdapter {
 
     private Context context;
-    private ArrayList<Sensores> dataSensores;
+    private ArrayList<Sensor> dataSensores;
     private LayoutInflater mInflaterCatalogListItems;
     ViewHolder holder;
 
 
-    public GridAdapter(Context context, ArrayList<Sensores> dataSensores){
+    public GridAdapter(Context context, ArrayList<Sensor> dataSensores){
         this.context = context;
         this.dataSensores = dataSensores;
         mInflaterCatalogListItems = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -47,9 +47,9 @@ public class GridAdapter extends BaseAdapter {
         if(convertView == null){
             holder = new ViewHolder();
             convertView = mInflaterCatalogListItems.inflate(adapter_sensor,null);
-            holder.Sensor = (TextView) convertView.findViewById(R.id.Sensor);
-            holder.Medida = (TextView) convertView.findViewById(R.id.Medida);
-            holder.linearLayout = (LinearLayout) convertView.findViewById(R.id.lyCelda);
+            holder.setLblSensor((TextView) convertView.findViewById(R.id.Sensor));
+            holder.setLblMedida((TextView) convertView.findViewById(R.id.Medida));
+            holder.setLinearLayout((LinearLayout) convertView.findViewById(R.id.lyCelda));
 
             convertView.setTag(holder);
          } else {
@@ -57,9 +57,9 @@ public class GridAdapter extends BaseAdapter {
         }
 
         if(dataSensores.get(position) != null){
-            holder.Sensor.setText(dataSensores.get(position).getSensorName());
-            holder.Medida.setText(dataSensores.get(position).getMedida());
-            holder.linearLayout.setBackgroundColor(dataSensores.get(position).getColor());
+            holder.getLblSensor().setText(dataSensores.get(position).getSensorName());
+            holder.getLblMedida().setText(dataSensores.get(position).getMedida());
+            holder.getLinearLayout().setBackgroundColor(dataSensores.get(position).getColor());
 
 
         }
@@ -67,14 +67,38 @@ public class GridAdapter extends BaseAdapter {
     }
 
     private static class ViewHolder {
-        TextView Sensor;
-        TextView Medida;
-        LinearLayout linearLayout;
+        private TextView lblSensor;
+        private TextView lblMedida;
+        private LinearLayout linearLayout;
+
+        public TextView getLblSensor() {
+            return lblSensor;
+        }
+
+        public void setLblSensor(TextView lblSensor) {
+            this.lblSensor = lblSensor;
+        }
+
+        public TextView getLblMedida() {
+            return lblMedida;
+        }
+
+        public void setLblMedida(TextView lblMedida) {
+            this.lblMedida = lblMedida;
+        }
+
+        public LinearLayout getLinearLayout() {
+            return linearLayout;
+        }
+
+        public void setLinearLayout(LinearLayout linearLayout) {
+            this.linearLayout = linearLayout;
+        }
     }
 
     public void setMedida(int position,String medida){
         dataSensores.get(position).setMedida(medida);
-        holder.Medida.setText(medida);
+        holder.getLblMedida().setText(medida);
     }
 
 }
